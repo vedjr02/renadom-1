4fa92ee5e07524b48ee8d8f0340b6d76ef00428e
Vedjr02 <ambreved3@gmail.com>
2026-05-25 16:40:17 +0100

    feat: add countActiveByZone selector for zone load metrics

--- lib/simulation/selectors.ts
+++ lib/simulation/selectors.ts
@@ -2,3 +2,8 @@ import type { ActiveOrder } from "./types";
 
 export const countBreachedOrders = (orders: ActiveOrder[]): number =>
   orders.filter((order) => order.breached).length;
+
+export const countActiveByZone = (
+  orders: ActiveOrder[],
+  zone: ActiveOrder["zone"],
+): number => orders.filter((order) => order.zone === zone).length;
