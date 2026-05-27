62198b7d24144b6e5ceb50299b7f2aa32b1763c7
Vedjr02 <ambreved3@gmail.com>
2026-05-27 21:51:36 +0100

    feat: add countExpressOrders selector for express lane totals

--- lib/simulation/selectors.ts
+++ lib/simulation/selectors.ts
@@ -24,3 +24,6 @@ export const getAverageUtilization = (zones: { utilization: number }[]): number 
 
 export const countStandardOrders = (orders: ActiveOrder[]): number =>
   orders.filter((order) => order.priority === "Standard").length;
+
+export const countExpressOrders = (orders: ActiveOrder[]): number =>
+  orders.filter((order) => order.priority === "Express").length;
