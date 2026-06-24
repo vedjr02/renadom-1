ad89588a1382f2079ff7fcd8d41b5e2a8c67d875
Vedjr02 <ambreved3@gmail.com>
2026-06-24 12:10:57 +0100

    feat(sim): add isVipOrder helper

--- lib/simulation/helpers.ts
+++ lib/simulation/helpers.ts
@@ -1,2 +1,3 @@
 import type { ActiveOrder, OrderPriority } from '@/lib/simulation/types';
 export const isExpressOrder = (o: ActiveOrder): boolean => o.priority === 'Express';
+export const isVipOrder = (o: ActiveOrder): boolean => o.priority === 'VIP';
