bb8087039f1dcf662606bc0f72f1a0bd4c81ae35
Vedjr02 <ambreved3@gmail.com>
2026-06-24 12:10:59 +0100

    feat(sim): add isBreached helper

--- lib/simulation/helpers.ts
+++ lib/simulation/helpers.ts
@@ -1,3 +1,4 @@
 import type { ActiveOrder, OrderPriority } from '@/lib/simulation/types';
 export const isExpressOrder = (o: ActiveOrder): boolean => o.priority === 'Express';
 export const isVipOrder = (o: ActiveOrder): boolean => o.priority === 'VIP';
+export const isBreached = (o: ActiveOrder): boolean => o.breached;
