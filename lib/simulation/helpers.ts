a03ffcd4f952c8806bb9140487f27617ebe2b520
Vedjr02 <ambreved3@gmail.com>
2026-06-24 12:11:02 +0100

    feat(sim): add isPacking helper

--- lib/simulation/helpers.ts
+++ lib/simulation/helpers.ts
@@ -3,3 +3,4 @@ export const isExpressOrder = (o: ActiveOrder): boolean => o.priority === 'Expre
 export const isVipOrder = (o: ActiveOrder): boolean => o.priority === 'VIP';
 export const isBreached = (o: ActiveOrder): boolean => o.breached;
 export const isPicking = (o: ActiveOrder): boolean => o.status === 'Picking';
+export const isPacking = (o: ActiveOrder): boolean => o.status === 'Packing';
